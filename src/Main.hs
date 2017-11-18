6984081740892f3b0ec8acebc5f2e024a221104c
Sean McGroarty <mcgroas@tcd.ie>
2017-11-18 12:47:33 +0000

    added: some new example

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,8 @@ import Shapes
 import Style
 import Colour
 
-exampleDrawing =  [ (scale 10 10 <+> rotate 90, circle, fill red <%> strokeWidth 0.5) ]
+exampleDrawing =  [ (scale 30 30 <+> translate 40 10, circle, fill red <%> strokeWidth 0.1), 
+                    (scale 50 50 <+> rotate 120, square, fill blue <%> strokeWidth 0.1 <%> stroke green)]
 
 main = serve $ render exampleDrawing
  
